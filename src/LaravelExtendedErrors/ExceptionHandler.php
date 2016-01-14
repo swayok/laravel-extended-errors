f480a52219a7132a2e182e832c19b8ca0e0568d2
Filippov Aleksandr <swayalex@gmail.com>
2016-01-14 11:34:50 +0300

    ExceptionsHandler - added try-catch to _convertExceptionToResponse() method

--- src/LaravelExtendedErrors/ExceptionHandler.php
+++ src/LaravelExtendedErrors/ExceptionHandler.php
@@ -16,7 +16,11 @@ class ExceptionHandler extends Handler {
     }
 
     protected function _convertExceptionToResponse(\Exception $exc) {
-        return (new ExceptionRenderer(config('app.debug')))->createResponse($exc);
+        try {
+            return (new ExceptionRenderer(config('app.debug')))->createResponse($exc);
+        } catch (\Exception $exc2) {
+            return parent::convertExceptionToResponse($exc2);
+        }
     }
 
     public function renderExceptionForEmail(Exception $exc) {
