86426f5baf210529a04aa1ef456ee7ea30829756
Filippov Aleksandr <swayalex@gmail.com>
2016-09-21 17:14:30 +0300

    Logger - added 'title' to $context so it can be used in email subject via %context.title% insert for usual logs and exceptions

--- src/LaravelExtendedErrors/Logger.php
+++ src/LaravelExtendedErrors/Logger.php
@@ -9,9 +9,13 @@ class Logger extends Monolog {
 
     public function addRecord($level, $message, array $context = array()) {
         if ($message instanceof \Exception) {
+            $context['title'] = $message->getMessage();
             $handler = new ExceptionHandler($this);
             $message = $handler->renderExceptionForLogger($message);
+        } else {
+            $context['title'] = $message;
         }
         return parent::addRecord($level, $message, $context);
     }
+
 }
