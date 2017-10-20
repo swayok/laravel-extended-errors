fd03b336c3f732372d01c100c306220e1a4452d0
Filippov Aleksandr <swayalex@gmail.com>
2017-10-20 15:15:22 +0300

    Improved Logger to detect exceptions in context (Laravel 5.5 compability)

--- src/LaravelExtendedErrors/Logger.php
+++ src/LaravelExtendedErrors/Logger.php
@@ -8,10 +8,12 @@ use Monolog\Logger as Monolog;
 class Logger extends Monolog {
 
     public function addRecord($level, $message, array $context = array()) {
-        if ($message instanceof \Exception) {
-            $context['title'] = $message->getMessage();
+        if ($message instanceof \Exception || array_get($context, 'exception', null) instanceof \Exception) {
+            /** @var \Exception $exception */
+            $exception = $message instanceof \Exception ? $message : $context['exception'];
+            $context['title'] = $exception->getMessage();
             $handler = new ExceptionHandler($this);
-            $message = $handler->renderExceptionForLogger($message);
+            $message = $handler->renderExceptionForLogger($exception);
         } else {
             $context['title'] = $message;
         }
